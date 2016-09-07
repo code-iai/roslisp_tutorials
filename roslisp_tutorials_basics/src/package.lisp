15bffd596885a81d04aed16b1cc5b447f17ba95f
Gayane Kazhoyan <kazhoyan@cs.uni-bremen.de>
2016-09-07 13:44:43 +0200

    in basic tutorial exported ADD-TWO-INTS-CLIENT-MAIN

--- roslisp_tutorials_basics/src/package.lisp
+++ roslisp_tutorials_basics/src/package.lisp
@@ -3,4 +3,5 @@
   (:export :talker :listener
            :array-talker :array-listener
            :rosout-example :params-example
-           :add-two-ints-server :add-two-ints-client))
+           :add-two-ints-server :add-two-ints-client
+           :add-two-ints-client-main))
